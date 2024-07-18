ffe7456ae80ce74b4361abc26da352264c234c57
Yashasvi Agrawal <122177943+yashasvii29@users.noreply.github.com>
2024-07-18 23:58:28 +0530

    update folder

---
+++ vite-project/src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>
+      <div>
+        <div className='h-screen items-center justify-center flex'>
+            <div className='w-3/5 h-2/5 md:w-2/5 bg-blue-400 sm:bg-red-400 md:bg-green-400 lg:bg-yellow-400 xl:bg-pink-400 2xl:bg-lime-400 '></div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default App
